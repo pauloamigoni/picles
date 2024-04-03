651ecbc3f4957dc973d8e9f53f56da00d6481e64
Paulo Amigoni <paulo.amigoni@gmail.com>
2024-04-02 22:22:23 -0300

    Adicionando add Patch

--- src/shelter/shelter.controller.ts
+++ src/shelter/shelter.controller.ts
@@ -1,7 +1,8 @@
-import { Controller, Get, Inject, Post } from '@nestjs/common';
+import { Body, Controller, Get, Inject, Patch, Post } from '@nestjs/common';
 import GetShelterDetailsUseCaseOutput from './usecases/dtos/get.shelter.details.usecase.output';
 import { IUseCase } from 'src/domain/iusecase.interface';
 import ShelterTokens from './shelter.tokens';
+import UpdateShelterControllerInput from './dtos/update.shelter.controller.input';
 
 @Controller('shelter')
 export class ShelterController {
@@ -14,5 +15,10 @@ export class ShelterController {
   @Get()
   async getShelterDetails(): Promise<GetShelterDetailsUseCaseOutput> {
     return await this.getShelterDetailsUseCase.run(null);
+  }
+
+  @Patch()
+  async updateShelterDetails(@Body() input: UpdateShelterControllerInput) {
+    console.log(input)
   }
 }
