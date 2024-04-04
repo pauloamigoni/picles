ddc90c3346d9fdbd8061c44915cb881f60e7a00b
Paulo Amigoni <paulo.amigoni@gmail.com>
2024-04-03 22:06:40 -0300

    Adicionando validation e transforme class-validation e transforme

--- src/shelter/dtos/update.shelter.controller.input.ts
+++ src/shelter/dtos/update.shelter.controller.input.ts
@@ -1,6 +1,25 @@
+import { IsEmail, IsNotEmpty, IsNumberString, IsString, Length, NotContains } from "class-validator"
+
 export default class UpdateShelterControllerInput {
+
+  @IsString()
+  @IsNotEmpty()
+  @NotContains(" ")
   name: string
+  @IsString()
+  @Length(10,11)
+  @IsNotEmpty()
   whatsapp: string
+  @IsString()
+  @IsNumberString()
+  @IsNotEmpty()
   phone: string
+  @IsString()
+  @IsEmail()
+  @IsNotEmpty()
   email: string
 }
+function IsAlphaNUmereic(): (target: UpdateShelterControllerInput, propertyKey: "name") => void {
+  throw new Error("Function not implemented.")
+}
+
