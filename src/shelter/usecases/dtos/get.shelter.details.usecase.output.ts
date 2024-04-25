c2620b8a5316d93a5c94ce2fc8a618ab906275b0
Paulo Amigoni <paulo.amigoni@gmail.com>
2024-04-25 16:25:09 -0300

    Ajustando o update do whatsapp e mundando para whatsApp

--- src/shelter/usecases/dtos/get.shelter.details.usecase.output.ts
+++ src/shelter/usecases/dtos/get.shelter.details.usecase.output.ts
@@ -1,6 +1,6 @@
 export default class GetShelterDetailsUseCaseOutput {
   shelterName: string;
-  shelterWhatsApp: string;
+  shelterWhatsapp: string;
   shelterEmail: string;
   shelterPhone: string;
   createdAt: Date;
